ef1fbcb3dbccded3751b8129859c8b16768927c2
ZachHung <zachhung911@gmail.com>
2022-03-30 12:37:15 +0700

    Adjusted API Call util

--- frontend/src/utils/CallApi.js
+++ frontend/src/utils/CallApi.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = `http://localhost:5000/api/`;
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 
-const user = JSON.parse(localStorage.getItem("persist:root")).user;
-const currentUser = user && JSON.parse(user).currentUser;
-const TOKEN = currentUser.accessToken;
+const user = JSON.parse(localStorage.getItem("persist:root"))
+  ? JSON.parse(localStorage.getItem("persist:root")).user
+  : undefined;
+const currentUser = user !== undefined && JSON.parse(user).currentUser;
+const TOKEN = user === undefined ? "" : currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -15,5 +17,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { Authorization: `Bearer ${TOKEN}` },
 });
