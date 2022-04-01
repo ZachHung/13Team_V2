b5f50fc9dad3c9457996b23c1f17699a8d7f4e1b
Your name <hoan42532@gmail.com>
2022-04-01 07:54:03 +0700

    add promotion slide component

--- frontend/src/components/swiper/Swiper.js
+++ frontend/src/components/swiper/Swiper.js
@@ -3,58 +3,114 @@ import 'swiper/css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Swiper.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
 export default () => {
   const [brand, setBrand] = useState([]);
-
+  const [checkedBrand, setCheckedBrand] = useState([]);
+  const [checkedPrice, setCheckedPrice] = useState([]);
   useEffect(() => {
     api.get('/phone/brand').then((res) => {
       console.log(res.data);
       setBrand(res.data);
     });
   }, []);
-  const handleOnClickSwiper = () => {
-    console.log('clicked: ');
+
+  const handleCheckBrand = (name) => {
+    setCheckedBrand((prev) => {
+      const isExist = checkedBrand.includes(name);
+      if (isExist) {
+        return checkedBrand.filter((item) => item !== name);
+      } else {
+        return [...prev, name];
+      }
+    });
   };
+  console.log(checkedBrand);
+  const handleCheckPrice = (name) => {
+    setCheckedPrice((prev) => {
+      const isExist = checkedPrice.includes(name);
+      if (isExist) {
+        return checkedPrice.filter((item) => item !== name);
+      } else {
+        return [...prev, name];
+      }
+    });
+  };
+  console.log(checkedPrice);
   return (
-    <>
+    <div className="filter-product">
       <Swiper
         className="brand-swiper"
         spaceBetween={10}
-        slidesPerView={3}
+        slidesPerView={5}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
         {brand.map((brand) => (
-          <SwiperSlide onClick={handleOnClickSwiper}>
+          <SwiperSlide onClick={() => handleCheckBrand(brand.name)}>
             <img
               src={`http://localhost:5000/${brand.brandImage}`}
               alt={`logo ${brand.name}`}
             ></img>
           </SwiperSlide>
         ))}
-        {/* <SwiperSlide onClick={handleOnClickSwiper}>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide> */}
       </Swiper>
       <Swiper
         className="price-swiper"
         spaceBetween={10}
-        slidesPerView={3}
+        slidesPerView={4}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        <SwiperSlide onClick={handleOnClickSwiper}>Dưới 2 triệu</SwiperSlide>
-        <SwiperSlide>Từ 2 đến 5 triệu</SwiperSlide>
-        <SwiperSlide>Từ 5 đến 14 triệu</SwiperSlide>
-        <SwiperSlide>Trên 14 triệu</SwiperSlide>
+        <SwiperSlide onClick={() => handleCheckPrice('duoi-2-trieu')}>
+          Dưới 2 triệu
+        </SwiperSlide>
+        <SwiperSlide onClick={() => handleCheckPrice('tu-2-5-trieu')}>
+          Từ 2 đến 5 triệu
+        </SwiperSlide>
+        <SwiperSlide onClick={() => handleCheckPrice('tu-5-14-trieu')}>
+          Từ 5 đến 14 triệu
+        </SwiperSlide>
+        <SwiperSlide onClick={() => handleCheckPrice('tren-14-trieu')}>
+          Trên 14 triệu
+        </SwiperSlide>
       </Swiper>
-    </>
+      <div>
+        <span style={{ fontWeight: 500, paddingRight: '1rem' }}>Lọc theo:</span>
+        {checkedBrand.map((item) => (
+          <span key={item} className="filter-span">
+            {item}
+            <FontAwesomeIcon
+              icon={faXmark}
+              style={{
+                paddingRight: '5px',
+                paddingLeft: '5px',
+                fontWeight: '300',
+              }}
+            />
+          </span>
+        ))}
+        {checkedPrice.map((item) => (
+          <span key={item} className="filter-span">
+            {item}
+            <FontAwesomeIcon
+              icon={faXmark}
+              style={{
+                paddingRight: '5px',
+                paddingLeft: '5px',
+                fontWeight: '300',
+              }}
+            />
+          </span>
+        ))}
+      </div>
+    </div>
   );
 };
