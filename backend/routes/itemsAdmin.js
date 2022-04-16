8f48675967c8cee39e056affc061c593b18de5b4
Tình Bùi <tinhbui127@gmail.com>
2022-04-16 22:03:57 +0700

    upload image to firebase by Tinh

--- backend/routes/itemsAdmin.js
+++ backend/routes/itemsAdmin.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const ItemController = require("../api/controllers/ItemController");
-const multer = require('multer')
-const upload = multer({ dest: "./backend/public/image/uploads/" })
 
 router.delete("/delete/:id", ItemController.deleteItemAdmin);
 router.delete("/deleteMany", ItemController.deleteManyItemsAdmin);
 router.get("/edit/:id", ItemController.edit);
-router.put("/update/:id", upload.single('upload'), ItemController.updateItem);
-router.put("/updateDetail/:id", upload.single('upload'),  ItemController.updateItemDetail);
+router.put("/update/:id", ItemController.updateItem);
+router.put("/updateDetail/:id", ItemController.updateItemDetail);
 router.get("/", ItemController.getItemsAdmin);
 router.post("/",ItemController.createPostItems);
 
