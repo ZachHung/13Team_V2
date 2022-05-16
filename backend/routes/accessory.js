f3f9d14bf25196fd5d974810d37c2d5217ca29c5
Tran Nhut <tnhut803@gmail.com>
2022-05-16 19:49:54 +0700

    add toast detail Item

--- backend/routes/accessory.js
+++ backend/routes/accessory.js
@@ -3,17 +3,10 @@ const router = express.Router();
 const itemController = require("../api/controllers/ItemController");
 const PhoneController = require("../api/controllers/PhoneController");
 
-<<<<<<< HEAD
-router.get("/brand", PhoneController.brand);
 router.get("/brand/name", PhoneController.brandName);
+router.get("/brand", PhoneController.brand);
 router.get("/:slug", itemController.detailItemAccessory);
-router.get("/", PhoneController.home);
-=======
-router.get('/brand/name', PhoneController.brandName);
-router.get('/brand', PhoneController.brand);
-router.get('/:slug', itemController.detailItemAccessory);
 
-router.get('/', PhoneController.home);
->>>>>>> b380853f2a6a879601248aae97bde25cf0cf796d
+router.get("/", PhoneController.home);
 
 module.exports = router;
