ae6019924f6a65698881026dbeafc6267caaffb0
Tình Bùi <tinhbui127@gmail.com>
2022-05-18 20:33:36 +0700

    update

--- backend/routes/purchasesAdmin.js
+++ backend/routes/purchasesAdmin.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const PurchaseController = require("../api/controllers/PurchaseController");
-// const {
-//     verifyTokenAdmin,
-//     verifyTokenAuth,
-// } = require('../middlewares/verificationHandler');
+const {
+    verifyTokenAdmin,
+    verifyTokenAuth,
+} = require('../middlewares/verificationHandler');
 
 router.delete("/delete/:id", PurchaseController.deletePurchasesAdmin);
 router.delete("/deleteMany", PurchaseController.deleteManyPurchasesAdmin);
 router.get("/detail/:id", PurchaseController.detailPurchasesAdmin);
-router.get("/edit/:id", PurchaseController.edit);
-router.put("/update/:id", PurchaseController.updatePurchase);
+router.get("/edit/:id", verifyTokenAdmin, PurchaseController.edit);
+router.put("/update/:id", verifyTokenAdmin, PurchaseController.updatePurchase);
 router.get("/", PurchaseController.getPurchasesAdmin);
 
 module.exports = router;
