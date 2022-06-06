603c16e84259ac61a1fda9d3cdb464c22fe5738c
Tran Nhut <tnhut803@gmail.com>
2022-06-06 21:26:27 +0700

    fix responsive

--- frontend/src/components/techInfo/TechInfo.jsx
+++ frontend/src/components/techInfo/TechInfo.jsx
@@ -3,12 +3,16 @@ import "./TechInfo.scss";
 const TechInfo = ({ demoArray }) => {
   return (
     <ul className="techInfo__container">
-      {demoArray.map((item, index) => (
-        <li className="techInfo__item row" key={index}>
-          <p className="techInfo__name col-4"> {item.infoName} </p>
-          <p className="techInfo__value col-8"> {item.infoNum}</p>
-        </li>
-      ))}
+      {demoArray.map((item, index) => {
+        if (index < 5) {
+          return (
+            <li className="techInfo__item row" key={index}>
+              <p className="techInfo__name col-4"> {item.infoName} </p>
+              <p className="techInfo__value col-8"> {item.infoNum}</p>
+            </li>
+          );
+        }
+      })}
     </ul>
   );
 };
